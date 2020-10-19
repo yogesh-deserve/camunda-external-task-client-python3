abe3178f1b61d441d30fabff8601cb73ef108e42
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-10-19 21:50:19 +0530

    Added more scenarios

--- examples/generic_bpmn_error_example.py
+++ examples/generic_bpmn_error_example.py
@@ -47,10 +47,15 @@ def raise_reject_error(task: ExternalTask):
     return task.bpmn_error("reject", "FRAUD")
 
 
+def close_application(task: ExternalTask):
+    return task.bpmn_error("close", "Product not accepted by user")
+
+
 def trigger_manual_review(task: ExternalTask):
     error_task_id = task._context["activityId"]
     return task.bpmn_error("manual_review", "Moving to Manual Review",
-                           {"errorTaskId": error_task_id, "reason": f"Manual review needed for {error_task_id} reason"})
+                           {"errorTaskId": error_task_id,
+                            "reason": f"Manual review needed for {error_task_id} reason"})
 
 
 def continue_next_task(task: ExternalTask):
@@ -74,33 +79,60 @@ def move_to_needs_info(task: ExternalTask):
     log_with_context(f"moving to needs_info: activity_error_task_id = {activity_error_task_id}, "
                      f"var_error_task_id = {var_error_task_id}, using error_task_id = {error_task_id}")
     return task.bpmn_error("needs_info", "moving to needs info for possible fraud",
-                               {"errorTaskId": error_task_id, "add_bank": True, "identity_docs": False, "student_id": True})
+                           {"errorTaskId": error_task_id, "add_bank": True, "identity_docs": False, "student_id": True})
 
 
 def main():
     configure_logging()
     # ----------------------------------------------------------------------------------------------------------------
-    # Scenario: Manual review triggered by XPN task and then Manual review task raises reject error
+    # Scenario: Manual review triggered by XPN task and then Manual review task raises reject error.
+    # Reject task should get triggered
     # http://localhost:8080/engine-rest/history/activity-instance?processInstanceId=d7a9d760-11ef-11eb-8a45-0242ac130003&sortBy=startTime&sortOrder=asc
     # topics = [
     #     (["REQUEST_IDV_DATA", "UPDATE_IDV_DATA",
     #       "REQUEST_CAPTURE_DATA", "UPDATE_CAPTURE_DATA",
-    #       "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"],
-    #      task_complete_success),
+    #       "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"], task_complete_success),
     #     ("XPN_CREDIT_PULL", trigger_manual_review),
     #     ("REQUEST_MANUAL_REVIEW", task_complete_success),
     #     ("UPDATE_MANUAL_REVIEW", raise_reject_error),
     # ]
     # ----------------------------------------------------------------------------------------------------------------
-    # Scenario: Manual review triggered by XPN task and then Manual review task results in "retry"
+    # Scenario: Manual review triggered by XPN task and then Manual review task results in "continue",
+    # this should result in continuing the task after XPN Task
+    # {{CAMUNDA_REST_URL}}/history/activity-instance?processInstanceId=da134b1b-11f1-11eb-8a45-0242ac130003&sortBy=startTime&sortOrder=asc
+    # topics = [
+    #     (["REQUEST_IDV_DATA", "UPDATE_IDV_DATA",
+    #       "REQUEST_CAPTURE_DATA", "UPDATE_CAPTURE_DATA",
+    #       "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"], task_complete_success),
+    #     ("XPN_CREDIT_PULL", trigger_manual_review),
+    #     ("REQUEST_MANUAL_REVIEW", task_complete_success),
+    #     ("UPDATE_MANUAL_REVIEW", continue_next_task),
+    # ]
+    # ----------------------------------------------------------------------------------------------------------------
+    # Scenario: Manual review triggered by XPN task and then workflow waits at Update Manual review task.
+    # REJECT_APPLICATION message is sent to reject the application.
+    # It should invoke the Reject application process, but instead getting error:
+    # org.camunda.bpm.engine.MismatchingMessageCorrelationException: Cannot correlate message 'REJECT_APPLICATION': No process definition or execution matches the parameters
+    # topics = [
+    #     (["REQUEST_IDV_DATA", "UPDATE_IDV_DATA",
+    #       "REQUEST_CAPTURE_DATA", "UPDATE_CAPTURE_DATA",
+    #       "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"], task_complete_success),
+    #     ("XPN_CREDIT_PULL", trigger_manual_review),
+    #     ("REQUEST_MANUAL_REVIEW", task_complete_success),
+    # ]
+    # ----------------------------------------------------------------------------------------------------------------
+    # Scenario: Update product acceptance raises "close" error to close the application
+    # topics = [
+    #     (["REQUEST_IDV_DATA", "UPDATE_IDV_DATA",
+    #       "REQUEST_CAPTURE_DATA", "UPDATE_CAPTURE_DATA",
+    #       "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"], task_complete_success),
+    #     ("XPN_CREDIT_PULL", trigger_manual_review),
+    # ]
+    # ----------------------------------------------------------------------------------------------------------------
     topics = [
-        (["REQUEST_IDV_DATA", "UPDATE_IDV_DATA",
-          "REQUEST_CAPTURE_DATA", "UPDATE_CAPTURE_DATA",
-          "REQUEST_CREDIT_PULL_CONSENT", "UPDATE_CREDIT_PULL_CONSENT"],
-         task_complete_success),
-        ("XPN_CREDIT_PULL", trigger_manual_review),
+        ("STEP_1", task_complete_success),
+        ("STEP_2", trigger_manual_review),
         ("REQUEST_MANUAL_REVIEW", task_complete_success),
-        ("UPDATE_MANUAL_REVIEW", retry_task),
     ]
     # ----------------------------------------------------------------------------------------------------------------
     # ("REQUEST_NEEDS_INFO", task_complete_success),
